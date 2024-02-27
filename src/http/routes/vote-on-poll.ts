f09ff495034cbeaee46611ef1a7be29e2a4a1d35
Vinih1237 <159467841+Vinih1237@users.noreply.github.com>
2024-02-27 17:37:11 +0000

    NLW Aula 3

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,6 +3,7 @@ import { randomUUID } from "crypto"
 import { prisma } from "../../lib/prisma"
 import { FastifyInstance } from "fastify"
 import { redis } from "../../lib/redis"
+import { voting } from "../../utils/voting-pub-sub"
 
 export async function voteOnPoll(app: FastifyInstance) {
 app.post('/polls/:pollId/votes', async (request, reply)=> {
@@ -34,7 +35,13 @@ app.post('/polls/:pollId/votes', async (request, reply)=> {
                 id: userPreviousVoteOnPoll.id,
             }
            })
-           await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+           const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+
+           voting.publish(pollId, {
+            pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+            votes: Number(votes),
+        })
+
         } else if (userPreviousVoteOnPoll){
             return reply.status(400).send({message: "You already voted on this poll!"})
         }
@@ -59,7 +66,12 @@ app.post('/polls/:pollId/votes', async (request, reply)=> {
             pollOptionId,
         }
     }) 
-    await redis.zincrby(pollId, 1, pollOptionId)
+   const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+    voting.publish(pollId, {
+        pollOptionId,
+        votes: Number(votes),
+    })
 
     return reply.status(201).send()
 })}
